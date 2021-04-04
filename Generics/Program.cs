239a09e9df760d44789b3cd119e87cdcef075543
Eloparto <kagan.celenk10@gmail.com>
2021-04-04 13:54:52 +0300

    Add project files.

---
+++ Generics/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> myList = new MyList<string>();
+            myList.Add("Test");
+            Console.WriteLine(myList.Count);
+        }
+    }
+
+    class MyList<T>
+    {
+        T[] _array;
+        public MyList()
+        {
+            _array = new T[0]; 
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = _array;
+            _array = new T[_array.Length+1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                _array[i] = tempArray[i];
+            }
+
+            _array[_array.Length - 1] = item;
+
+        }
+
+
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+
+    }
+
+}
